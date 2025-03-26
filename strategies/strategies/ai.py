51af0cda1e1c954920b5bb21eac1939b60dab38f
Pedro Sousa <pedronsousa@gmail.com>
2025-03-26 17:28:41 +0000

    add debug to predicted card

--- strategies/strategies/ai.py
+++ strategies/strategies/ai.py
@@ -4,7 +4,7 @@ from transformer.transformer_model import HeartsTransformerModel
 from transformer.inputs import card_from_token
 import numpy as np
 
-DEBUG = False
+DEBUG = True
 
 def debug_print(message):
     if DEBUG:
@@ -18,12 +18,24 @@ class AIStrategy(Strategy):
 
     def choose_card(self, strategy_game_state: StrategyGameState) -> Card:
         predictions = self.model.predict(strategy_game_state.game_state)
+        
         ordered_predicted_cards = [
-            card_from_token(i)
+            (card_from_token(i), predictions[0][i])
             for i in np.argsort(predictions[0])[-52:][::-1]
         ]
-        for card in ordered_predicted_cards:
-            if card in strategy_game_state.valid_moves:
-                debug_print(f"Chosen card: {card}")
-                return card
+        debug_print("\nTop most probable cards:")
+        for card, prob in ordered_predicted_cards:
+            debug_print(f"{card} -> {prob * 100:.2f}% {'*' if card in strategy_game_state.valid_moves else ''}")
+        
+        valid_predicted_cards = [
+            (card, prob)
+            for card, prob in ordered_predicted_cards
+            if card in strategy_game_state.valid_moves
+        ]
+        # debug_print("\nTop most probable valid cards:")
+        # for card, prob in valid_predicted_cards:
+        #     debug_print(f"{card} -> {prob * 100:.2f}%")
+        if valid_predicted_cards:
+            # debug_print(f"Chosen card: {valid_predicted_cards[0][0]} with probability {valid_predicted_cards[0][1] * 100:.2f}%")
+            return valid_predicted_cards[0][0]
         raise ValueError("No valid moves predicted")
